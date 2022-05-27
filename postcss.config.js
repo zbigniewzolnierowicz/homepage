d24279111ff45066d6a2b037d80b9784380e35d8
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-05-27 13:56:56 +0200

    Things

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 const postcssPresetEnv = require('postcss-preset-env');
+const postcssLogical = require('postcss-logical');
 
 module.exports = {
     plugins: [
+        postcssLogical(),
         postcssPresetEnv({
             stage: 2,
             features: {
